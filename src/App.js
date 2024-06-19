1d0bbd7df027d9ed9fc489f7da87f357f6334a0b
SandhyaReddy <sandhya23@navgurukul.org>
2024-06-19 11:24:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useMemo, useState } from "react"
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [Time, ~] = useState(new Date().toLocaleTimeString());
+
+    const [products, setProducts] = useState(
+        [
+            { id: 1, name: 'One Plus Nord CE2', price: 20000 },
+            { id: 2, name: 'Oppo F21 S', price: 22000 },
+            { id: 3, name: 'Samsung Galaxy  M 33', price: 17000 },
+        ]
+    )
+
+
+    const [searchText, updateSearchText] = useState('');
+
+    const filteredProducts = useMemo(
+
+        () => {
+
+            console.log(products)
+
+            return products.filter(
+                (p) => {
+
+                    return p.name.toLowerCase().includes(searchText.toLowerCase())
+
+                }
+            )
+
+        },
+        [products, searchText]
+
+    )
+
+
+    useEffect(
+        () => {
+            setInterval(
+                () => {
+                    setTime(new Date().toLocaleTimeString())
+
+                }, 1000
+            )
+
+        }, []
+    )
+
+    return (
+        <>
+
+            <div>{Time}</div>
+
+            <input type="text" value={searchText} onChange={e => updateSearchText(e.target.value)} />
+
+            <table>
+                <tr>
+                    <td> S.No</td>
+                    <td>Name</td>
+                    <td>Price</td>
+                </tr>
+                {
+                    filteredProducts.map(
+                        p => (
+                            <tr key={p.id}>
+                                <td> {p.id}</td>
+                                <td>{p.name}</td>
+                                <td>{p.price}</td>
+                            </tr>
+
+                        )
+                    )
+                }
+            </table>
+        </>
+    )
+
 }
 
-export default App;
+export default App
